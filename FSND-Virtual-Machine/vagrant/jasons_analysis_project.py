6426051ad139343212521c52a4cf6cfd59f6d441
Jason Marsh <regularly.jason@gmail.com>
2018-02-03 15:33:00 -0600

    Functional query 3

--- FSND-Virtual-Machine/vagrant/jasons_analysis_project.py
+++ FSND-Virtual-Machine/vagrant/jasons_analysis_project.py
@@ -1,12 +1,11 @@
-import psycopg2
-import time 
 import sys
+import time
+import psycopg2
 
-"""Ensure user uses python 3 or greater"""
+"""Ensure user is running script with python 3 or greater"""
 
 if sys.version_info[0] < 3:
     raise Exception("A minimum of Python version 3 is required to run.")
-
 
 """Prepare SQL Views"""
 
@@ -15,13 +14,26 @@ view_top_slugs = ("CREATE OR REPLACE VIEW top_slugs_view AS SELECT path, \
                     WHERE status = '200 OK'                              \
                     AND NOT path = '/'                                   \
                     GROUP BY path ORDER BY num_views DESC;")
+
 alt_view = ("CREATE OR REPLACE VIEW alt_view AS SELECT author, num_views \
             FROM articles, top_slugs_view                                \
             WHERE top_slugs_view.path = CONCAT('/article/', slug)        \
             ORDER BY num_views DESC;")
+
 x_view = ("CREATE OR REPLACE VIEW x_view AS SELECT author, sum(num_views)           \
             FROM alt_view                                                \
             GROUP BY author;")
+
+errors_view = ("CREATE OR REPLACE VIEW errors_day_view AS SELECT     \
+                   DATE(time), COUNT(*) AS num_views                     \
+                   FROM log                                              \
+                   WHERE status = '404 NOT FOUND'                        \
+                   GROUP BY status, DATE                                 \
+                   ORDER BY num_views DESC;")
+
+hits_view = ("CREATE OR REPLACE VIEW hits_day_view AS SELECT         \
+                 DATE(time), COUNT(*) AS num_views                       \
+                 FROM log GROUP BY DATE(time);")
 
 """Prepare Query answers"""
 
@@ -33,7 +45,17 @@ answer_1 = ('SELECT title, num_views                                     \
 answer_2 = ('SELECT authors.name, x_view.sum                             \
             FROM x_view, authors                                         \
             WHERE x_view.author = authors.id                             \
-            ORDER BY sum DESC LIMIT 3;')
+            ORDER BY sum DESC LIMIT 31;')
+
+answer_3 = ('SELECT hits_day_view.date,                                  \
+            ROUND(SUM(CAST(errors_day_view.num_views AS decimal)          \
+            /                                                            \
+            CAST(hits_day_view.num_views as decimal) * 100), 2)          \
+            AS bad_requests                                              \
+            FROM hits_day_view, errors_day_view                           \
+            WHERE hits_day_view.date = errors_day_view.date               \
+            GROUP BY hits_day_view.date                                  \
+            ORDER BY bad_requests DESC LIMIT 1;')
 
 def connect(database_name="news"):
     """Prepare database if it exists and create necessary views."""
@@ -43,24 +65,28 @@ def connect(database_name="news"):
         cursor.execute(view_top_slugs)
         cursor.execute(alt_view)
         cursor.execute(x_view)
+        cursor.execute(errors_view)
+        cursor.execute(hits_view)
         return db, cursor
     except:
         print("<Error, no database found.>")
 
 def slowPrint(string):
+    """Slow down text printed to user."""
     for character in string:
         print(character)
         time.sleep(.05)
 
-welcome_banner = ('',
-                  '-' * 50,
-                  'Welcome to Jason\'s Python Database Query Machine!',
-                  '-' * 50,)
-help_prompt = "Type \"help\" for available commands."
+def printDivider(x):
+    print('-' * len(x))
+    print(x)
+    print('-' * len(x))
+
+welcome_banner = ('Welcome to Jason\'s Python Database Query Machine!')
 
 text_menu = ('1) Top 3 Articles',
              '2) Top Authors',
-             '3) Unfinished',
+             '3) Days With Greatest Percent Request Errors',
              '4) Exit')
 
 def queryTop3Articles():
@@ -68,7 +94,7 @@ def queryTop3Articles():
     db, cursor = connect()
     cursor.execute(answer_1)
     results = cursor.fetchall()
-    slowPrint(results)
+    cleanPrint(results)
     db.close()
 
 def queryTop3Authors():
@@ -76,39 +102,49 @@ def queryTop3Authors():
     db, cursor = connect()
     cursor.execute(answer_2)
     results = cursor.fetchall()
-    slowPrint(results)
+    cleanPrint(results)
+    db.close()
+
+def cleanPrint(y):
+    for x in y:
+        print('|', x[0], x[1], '|')
+
+def queryTopRequestErrors():
+    """Return the 3 most popular authors."""
+    db, cursor = connect()
+    cursor.execute(answer_3)
+    results = cursor.fetchall()
+    cleanPrint(results)
     db.close()
 
 def prompt_user():
     """Display main command prompt used for interacting with program."""
-    print(help_prompt)
+    print()
     user_input = input("Select an option 1-4: ")
 
     if user_input == '1':
-        print('')
-        print("Preparing top 3 articles based on views...")
-        print('')
+        printDivider("Preparing top 3 articles based on views...")
         time.sleep(.2)
         queryTop3Articles()
     elif user_input == '2':
-        print('')
-        print("Preparing Top 3 authors based on views...")
-        print('')
+        printDivider("Preparing Top 3 authors based on views...")
         time.sleep(.2)
         queryTop3Authors()
     elif user_input == '3':
-        print("This query is not completed yet.")
+        printDivider("Preparing data on days where more than one percent of " + 
+                     "requests lead to errors...")
+        time.sleep(.2)
+        queryTopRequestErrors()
     elif user_input == '4':
         print("Now quitting program...")
         time.sleep(.2)
         print("Program exited.") 
-        return   
+        return
     else:
         print("Not a recognized command.")
-
     prompt_user()
 
-slowPrint(welcome_banner)       # Print program start banner to user
+printDivider(welcome_banner)    # Print program start banner to user
 slowPrint(text_menu)            # Display available commands in the menu
 prompt_user()                   # Accept user commands
 
